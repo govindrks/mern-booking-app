b07a0ea1782797e1751d6658f8f3ac9822839dc6
Govind <govind151201@gmail.com>
2023-12-29 19:48:51 +0530

    basic setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,12 +1,15 @@
 import express, { Request, Response } from 'express'
 import cors from 'cors'
 import 'dotenv/config'
+import mongoose from 'mongoose'
+
+mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string)
 
 const app = express();
 // converts body of api request into json
 app.use(express.json())
 // allows requests to be made from different origins
-app.use(express.urlencoded({extended: true}))
+app.use(express.urlencoded({ extended: true }))
 // cross origin request for security issue
 app.use(cors())
 
