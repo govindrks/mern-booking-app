1fcc9154a30811c2fb12a15755124f7561d5f3b9
Govind <govind151201@gmail.com>
2023-12-29 19:11:30 +0530

    backend setup

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,7 +1,19 @@
-import express from 'express'
+import express, { Request, Response } from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 
 const app = express();
 // converts body of api request into json
-app.use
+app.use(express.json())
+// allows requests to be made from different origins
+app.use(express.urlencoded({extended: true}))
+// cross origin request for security issue
+app.use(cors())
+
+app.get("/api/test", async (req: Request, res: Response) => {
+   res.json({ message: "hello from express endpoint!" })
+});
+
+app.listen(7000, () => {
+    console.log("Server is running on port 7000");
+})
