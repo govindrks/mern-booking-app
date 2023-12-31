ab4694db70d265ebe1827a1d4aaaecc0a05ab288
Govind <govind151201@gmail.com>
2023-12-31 16:38:57 +0530

    registration page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,13 +4,22 @@ import {
   Routes,
 } from "react-router-dom";
 import Layout from "./layouts/Layout";
+import Register from "./pages/Register";
 
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/" element={<Layout><p>Home Page</p></Layout>} />
+        <Route
+        path="/"
+        element={
+          <Layout>
+            Home Page
+          </Layout>
+        }
+        />
+        <Route path="/register" element={<Layout><Register /></Layout>} />
       </Routes>
     </Router>
   )
